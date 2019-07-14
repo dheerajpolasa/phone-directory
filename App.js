f8de8b59e9c3bcf0417ac874e15325157048149f
dheerajpolasa <dheerajpolasa40@gmail.com>
2019-07-14 12:08:26 +0530

    Created Skeleton of Home Page

--- App.js
+++ App.js
@@ -3,8 +3,17 @@ import React from 'react';
 
 function App() {
   return (
-    <div >
-      Phone directory
+    <div>
+      {/* <div className="header">
+        Phone directory
+    </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br></br>
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name</label>
+      <input id="name" type="text" placeholder="Type in Here" defaultValue="Dheeraj"/>
     </div>
   );
 }
